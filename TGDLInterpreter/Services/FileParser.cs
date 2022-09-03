89e0f106745a61e50300750af997fbee25e0e1f9
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2022-09-03 12:02:30 +0200

    Add project files.

---
+++ TGDLInterpreter/Services/FileParser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace TGDLInterpreter.Services
+{
+    internal class TGDLParser
+    {
+        public void Parse(IBrowserFile file)
+        {
+
+        }
+    }
+}
