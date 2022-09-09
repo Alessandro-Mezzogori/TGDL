9f1e2f6793552de99f98a2fab5dc9ef3a47e01a9
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2022-09-09 17:44:46 +0200

    [Syntax] Added initializing value for state attribute

--- TGDLLib/Syntax/AttributeSyntaxDeclaration.cs
+++ TGDLLib/Syntax/AttributeSyntaxDeclaration.cs
@@ -1,7 +1,16 @@
-﻿namespace TGDLLib.Syntax;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TGDLLib.Syntax;
 
 public class AttributeSyntaxDeclaration
 {
-    public TypeSyntax Type { get; }
+    // type is derived from initializing value
     public IdentifierSyntaxToken Identifier { get; }
+    public LiteralExpressionSyntax InitializingValue { get; }  // TODO Support for initialiazation with attribute access
+
+    public AttributeSyntaxDeclaration(IdentifierSyntaxToken identifier, LiteralExpressionSyntax initializingValue)
+    {
+        Identifier = identifier;
+        InitializingValue = initializingValue;
+    }
 }
