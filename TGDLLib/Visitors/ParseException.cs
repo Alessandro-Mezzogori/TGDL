133ccb124a32e52c05354b9e33eb0e42ca05ea13
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2022-09-16 02:17:25 +0200

    Added ParseException to propagate errors from the visitors

---
+++ TGDLLib/Visitors/ParseException.cs
@@ -0,0 +1,20 @@
+﻿namespace TGDLLib.Visitors;
+
+public class ParseException : Exception
+{
+    private const string _defaultMessage = "Parsing error";
+    public ParseException(Type type) : base(_defaultMessage + " " + type.FullName)
+    {
+
+    }
+
+    public ParseException(Type type, string? message) : base(_defaultMessage + " " + type.FullName + ": " + message)
+    {
+    
+    }
+
+    public ParseException(string message): base(_defaultMessage + " " + message)
+    {
+
+    }
+}
